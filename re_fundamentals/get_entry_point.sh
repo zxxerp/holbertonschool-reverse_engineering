d712b99e9fa7725ea225cbcb048b9409c4ba8890
zxxerp <ayxanhuseynzade12@gmail.com>
2026-07-31 16:54:58 +0400

    Update get_entry_point.sh

--- re_fundamentals/get_entry_point.sh
+++ re_fundamentals/get_entry_point.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
 if [ $# -ne 1 ]; then
-    echo "Usage: $0 <ELF_file>"
     exit 1
 fi
 
 file_name="$1"
 
 if [ ! -f "$file_name" ]; then
-    echo "Error: File '$file_name' does not exist."
     exit 1
 fi
 
 if ! file "$file_name" | grep -q "ELF"; then
-    echo "Error: '$file_name' is not an ELF file."
     exit 1
 fi
 
@@ -21,7 +18,7 @@ source ./messages.sh
 
 header=$(readelf -h "$file_name")
 
-magic_number=$(echo "$header" | awk -F': *' '/Magic:/ {print $2}')
+magic_number=$(echo "$header" | awk -F': *' '/Magic:/ {print $2}' | sed 's/[[:space:]]*$//')
 class=$(echo "$header" | awk '/Class:/ {print $2}')
 byte_order=$(echo "$header" | awk -F', ' '/Data:/ {print $2}')
 entry_point_address=$(echo "$header" | awk '/Entry point address:/ {print $4}')
