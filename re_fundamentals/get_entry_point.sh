19aee505d98175fd6c76ffc028fac8ff9431a5a7
zxxerp <ayxanhuseynzade12@gmail.com>
2026-07-31 16:53:52 +0400

    Update get_entry_point.sh

--- re_fundamentals/get_entry_point.sh
+++ re_fundamentals/get_entry_point.sh
@@ -23,7 +23,7 @@ header=$(readelf -h "$file_name")
 
 magic_number=$(echo "$header" | awk -F': *' '/Magic:/ {print $2}')
 class=$(echo "$header" | awk '/Class:/ {print $2}')
-byte_order=$(echo "$header" | awk -F': *' '/Data:/ {print $2}')
+byte_order=$(echo "$header" | awk -F', ' '/Data:/ {print $2}')
 entry_point_address=$(echo "$header" | awk '/Entry point address:/ {print $4}')
 
 display_elf_header_info
